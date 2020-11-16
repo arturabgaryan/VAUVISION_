986def927332d33000056e148a8f7d07cf168c36
arturabgaryan <ar12.abgaryan@gmail.com>
2020-11-16 20:42:54 +0300

    16 nov commit

--- MusicSite/Music/test.py
+++ MusicSite/Music/test.py
@@ -1,5 +1,15 @@
-import subprocess
-import os
-path = '{}/static/documents/offer__vauvision__27102020-3.docx'.format(str(os.path.abspath('')))
-output = subprocess.check_output(['libreoffice', '--convert-to', 'pdf' ,path])
-os.rename('offer__vauvision__27102020-3.pdf','static/documents/offer__vauvision__27102020-3.pdf')
+from datetime import datetime
+import time
+
+print(time.ctime())
+name = input('Введите свой логин: ')
+time_list = time.localtime()
+hours = time_list[3]
+if 5 <= int(hours) <= 12:
+    print("Доброго утра,", name)
+elif 12 <= int(hours) <= 18:
+    print("Доброго дня,", name)
+elif 18 <= int(hours) <= 22:
+    print("Доброго вечера,", name)
+else:
+    print("Доброй ночи,", name)
