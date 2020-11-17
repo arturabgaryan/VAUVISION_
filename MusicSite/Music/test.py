5ff63f3fff7b4ee1b946083f2da405399e9a90be
arturabgaryan <ar12.abgaryan@gmail.com>
2020-11-17 23:00:37 +0300

    17 nov

--- MusicSite/Music/test.py
+++ MusicSite/Music/test.py
@@ -1,15 +1,4 @@
 from datetime import datetime
 import time
-
-print(time.ctime())
-name = input('Введите свой логин: ')
-time_list = time.localtime()
-hours = time_list[3]
-if 5 <= int(hours) <= 12:
-    print("Доброго утра,", name)
-elif 12 <= int(hours) <= 18:
-    print("Доброго дня,", name)
-elif 18 <= int(hours) <= 22:
-    print("Доброго вечера,", name)
-else:
-    print("Доброй ночи,", name)
+import os
+os.rename(f'{str(os.path.abspath("")[:-6])}/offer__vauvision__17112020-1.pdf', f'{str(os.path.abspath("")[:-6])}/Music/static/documents/offer__vauvision__17112020-1.pdf')
