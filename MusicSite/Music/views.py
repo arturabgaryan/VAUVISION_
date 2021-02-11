52173b7fd7828625508e1339c10df29bd9592441
arturabgaryan <ar12.abgaryan@gmail.com>
2021-02-11 21:06:04 +0300

    December

--- MusicSite/Music/views.py
+++ MusicSite/Music/views.py
@@ -218,7 +218,7 @@ def index(request):
     APP_TOKEN = 'AgAAAAA_8uwPAAarbHv2-khOnkCRmzitHRkTKdU'
     y = yadisk.YaDisk(token=APP_TOKEN)
     files = request.FILES.getlist('file1')
-    attached_file1 = files['file1']
+    attached_file1 = files[0]
     print(attached_file1)
     print(request.is_ajax())
     file = request.FILES.getlist('file1')
