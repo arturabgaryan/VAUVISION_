b45e5a94cd006e2447072e42801f55a4cb395042
arturabgaryan <ar12.abgaryan@gmail.com>
2021-02-18 19:38:48 +0300

    commited

--- MusicSite/Music/views.py
+++ MusicSite/Music/views.py
@@ -294,6 +294,22 @@ def index(request):
 
     name = request.POST.get('releaseName', None).replace(
                     " ", "__"),
+    
+    try:
+        pasp_info = PaspInfo.objects.get(email=email)
+    except:
+        paspinfo = PaspInfo.objects.create(
+            full_name=request.POST.get('FULLNAME', None),
+            who_given=request.POST.get('GIVEN_BY', None),
+            when_given=request.POST.get('GIVEN_DATE', None),
+            data_born=request.POST.get('BIRTH_DATE', None),
+            place_born=request.POST.get('REGISTRATION', None),
+            grajdanstvo=request.POST.get('COUNTRY', None),
+            seria_num=request.POST.get('SERIE_NUM', None),
+            artist_name=request.POST.get('artistName', None),
+            email=email
+        )
+        paspinfo.save()
 
     if name not in [directory.name for directory in list(
         y.listdir('/ДИСТРИБУЦИЯ VAUVISION/Заявки на загрузку/')
@@ -361,21 +377,7 @@ def index(request):
 Исполнитель: {track.singer}\n\n"""
 
 
-            try:
-                pasp_info = PaspInfo.objects.get(email=email)
-            except:
-                paspinfo = PaspInfo.objects.create(
-                    full_name=request.POST.get('FULLNAME', None),
-                    who_given=request.POST.get('GIVEN_BY', None),
-                    when_given=request.POST.get('GIVEN_DATE', None),
-                    data_born=request.POST.get('BIRTH_DATE', None),
-                    place_born=request.POST.get('REGISTRATION', None),
-                    grajdanstvo=request.POST.get('COUNTRY', None),
-                    seria_num=request.POST.get('SERIE_NUM', None),
-                    artist_name=request.POST.get('artistName', None),
-                    email=email
-                )
-                paspinfo.save()
+
 
         info_to_file += f"""
 Паспортные данные:
