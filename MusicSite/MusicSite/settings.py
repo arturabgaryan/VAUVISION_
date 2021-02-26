f7513b252a6c45c9e204eb7564017bb2e1425120
arturabgaryan <ar12.abgaryan@gmail.com>
2021-02-26 20:00:39 +0300

    sdf

--- MusicSite/MusicSite/settings.py
+++ MusicSite/MusicSite/settings.py
@@ -119,6 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 31457280
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
